2d4e2753b860bbe1df33ce72799d733792718770
hvu26510 <hvu26510@gmail.com>
2023-05-23 19:16:31 +0700

    Add project files.

---
+++ ArrayDaChieu/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDaChieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] array2chieu = new string[,] { { "a", "b", "c" }, 
+                {"x", "y", "z"}, { "k", "l","m"} };
+
+            
+
+            for(int row = 0; row < array2chieu.GetLength(0); row++) //GetLength(0) = so dong
+            {
+                for(int col = 0; col<array2chieu.GetLength(1); col++) //GetLength(1) = so cot
+                {
+                    string x = array2chieu[row, col];
+                    Console.Write(x + "     ");
+                }
+
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ JaggedArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] jaggedArray = new string[3][];
+
+            jaggedArray[0] = new string[] {"a", "b", "c"};
+            jaggedArray[1] = new string[] { "k", "h", "l", "m","p" };
+            jaggedArray[2] = new string[] { "z"};
+
+            for (int row = 0; row < jaggedArray.Length; row++)//jaggedArray.Length: lay so cot
+            {
+                for (int col = 0; col < jaggedArray[row].Length; col++) //jaggedArray[row].Length = So cot o hang row
+                {
+                    Console.Write(jaggedArray[row][col] + "     ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
